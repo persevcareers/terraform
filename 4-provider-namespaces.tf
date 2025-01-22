0c1b7442c16469cc036833f90e244c82b8698fb5
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-22 16:30:18 +0530

    Update 4-provider-namespaces.tf

--- 4-provider-namespaces.tf
+++ 4-provider-namespaces.tf
@@ -1,3 +1,14 @@
 provider "digitalocean" {
 
 }
+==============================
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.47.0"
+    }
+  }
+}
+
