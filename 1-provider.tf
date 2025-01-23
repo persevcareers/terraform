b4c3758aee6c355c5581db0b5436db7397d1646b
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-23 12:46:36 +0530

    Update 1-provider.tf

--- 1-provider.tf
+++ 1-provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA3ISBV2ZXATWPGPOU"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 resource "aws_instance" "automationserver" {
-  ami           = ami-00bb6a80f01f03502
+  ami           = "ami-00bb6a80f01f03502"
   instance_type = "t2.micro"
 }
