19fdc194a28be7652bd39d8ae5584905ffdee0aa
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-25 12:09:59 +0530

    Update 8-publicip.tf

--- 8-publicip.tf
+++ 8-publicip.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = ""
-  secret_key = ""
+# Create Elastic IP
+resource "aws_eip" "example" {
+   domain   = "vpc"
 }
 
-resource "aws_eip" "lb" {
-  domain   = "vpc"
+# Output the Elastic IP Address
+output "elastic_ip_address" {
+  value = aws_eip.example.public_ip
 }
