663e5296daf5d91e2d2d9b2ad70a30e89cb682b3
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-30 13:22:37 +0530

    Update 15-withoutcount.tf

--- 15-withoutcount.tf
+++ 15-withoutcount.tf
@@ -6,30 +6,26 @@ provider "aws" {
 
 resource "aws_instance" "web1" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = ""t2.micro
+  instance_type = "t2.micro"
   }
 
 resource "aws_instance" "web2" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = ""t2.micro
+  instance_type = "t2.micro"
   }
 
 
 resource "aws_instance" "web3" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = ""t2.micro
+  instance_type = "t2.micro"
   }
 
 resource "aws_instance" "web4" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = ""t2.micro
+  instance_type = "t2.micro"
   }
 
 resource "aws_instance" "web5" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = ""t2.micro
+  instance_type = "t2.micro"
   }
-
-
-
-
