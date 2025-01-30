09fd02ff30c610f4f5518b7f959e66c5e52ba4d3
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-30 12:58:53 +0530

    Create 14-datatype.tf

---
+++ 14-datatype.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA3ISBV2ZXIZHR3CBA"
+  secret_key = "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"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-00bb6a80f01f03502"
+  }
+
+variable "instance_type" {
+    type = number
+}
