3a1602ae56e401044dd357c6db7792e1ffae5bab
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-31 15:42:21 +0530

    Update 20-locals.tf

--- 20-locals.tf
+++ 20-locals.tf
@@ -18,6 +18,7 @@ resource "aws_security_group" "sg02" {
 name = "userrules"
 tags = var.tag
 }
+=============================================================
 # Locals block
 provider "aws" {
   region     = "ap-south-1"
@@ -28,6 +29,28 @@ locals {
 default = {
   app = "Automationnew"
 }
+}
+resource "aws_security_group" "sg01" {
+name = "applicationrules"
+tags = local.default
+}
+resource "aws_security_group" "sg02" {
+name = "userrules"
+tags = local.default
+}
+=============================================================
+FINAL EXECUTION
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA3ISBV2ZXIZHR3CBA"
+  secret_key = "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"
+}
+
+locals {
+default = {
+  app = "Automationnew"
+  deployeddate = "${formatdate("DDMMYYYY",timestamp())}"
+  }
 }
 resource "aws_security_group" "sg01" {
 name = "applicationrules"
