4a03c826a4c6f02c2f03e84b6f5bb406aba3c3fc
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-01-31 15:35:10 +0530

    Create 20-locals.tf

---
+++ 20-locals.tf
@@ -0,0 +1,39 @@
+# Execute variable block first
+# variable block
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA3ISBV2ZXIZHR3CBA"
+  secret_key = "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"
+}
+variable "tag" {
+default = {
+  app = "Automation"
+}
+}
+resource "aws_security_group" "sg01" {
+name = "applicationrules"
+tags = var.tag
+}
+resource "aws_security_group" "sg02" {
+name = "userrules"
+tags = var.tag
+}
+# Locals block
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA3ISBV2ZXIZHR3CBA"
+  secret_key = "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"
+}
+locals {
+default = {
+  app = "Automationnew"
+}
+}
+resource "aws_security_group" "sg01" {
+name = "applicationrules"
+tags = local.default
+}
+resource "aws_security_group" "sg02" {
+name = "userrules"
+tags = local.default
+}
