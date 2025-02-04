3d2c237bbdac8275479e84d9af8586dd6e8ff38f
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-04 14:38:13 +0530

    Create 21-datasources.tf

---
+++ 21-datasources.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA3ISBV2ZXIZHR3CBA"
+  secret_key = "H9ixW1Eychi9CcuH2PNGlfPrPZbQq51T6Ub9xkaE"
+}
+ 
+
+ data "aws_vpc" "samplevpc" {
+
+ }
