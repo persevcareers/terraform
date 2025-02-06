c3789a60faaf5344795a42839cb3ce320ed808b8
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-06 14:51:22 +0530

    Update 23-tainting.tf

--- 23-tainting.tf
+++ 23-tainting.tf
@@ -1,9 +1,15 @@
-terraform taint <resource_type>.<resource_name>
-
-terraform taint aws_instance.web
-
-terraform -replace <resource_type>.<resource_name>
-
-
-
-
+#Deploy ec2 via terraform
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+resource "aws_instance" "web" {
+  ami           = "ami-00bb6a80f01f03502"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "automationserver"
+  }
+}
+#Recreate it using terraform
+terraform apply -replace "aws_instance.web"
