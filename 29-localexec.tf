28382eb7ffaf8518917d856b2db676ffa36eb5bc
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-13 15:54:14 +0530

    Create 29-localexec.tf

---
+++ 29-localexec.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAYLZZKG25CI677AWM"
+  secret_key = "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"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-00bb6a80f01f03502"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo 'Welcome to automation'"
+  }
+}
