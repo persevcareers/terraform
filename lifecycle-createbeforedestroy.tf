6b5988deff4ac7c6d6598a04797bee19b096cbd5
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-13 14:44:38 +0530

    Update lifecycle-createbeforedestroy.tf

--- lifecycle-createbeforedestroy.tf
+++ lifecycle-createbeforedestroy.tf
@@ -20,3 +20,18 @@ resource "aws_instance" "automationserver" {
   ami           = "ami-00bb6a80f01f03502"
   instance_type = "t2.micro"
 }
+
+=======================================================
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAYLZZKG25CI677AWM"
+  secret_key = "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"
+}
+resource "aws_instance" "automationserver" {
+  ami           = "ami-02ddb77f8f93ca4ca"
+  instance_type = "t2.micro"
+  lifecycle {
+    create_before_destroy = true
+  } 
+}
