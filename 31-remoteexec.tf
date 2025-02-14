9b57846988ba509c7b836f633286c92f43ca63e1
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-14 17:03:47 +0530

    Update 31-remoteexec.tf

--- 31-remoteexec.tf
+++ 31-remoteexec.tf
@@ -1,1 +1,21 @@
 
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAYLZZKG25CI677AWM"
+  secret_key = "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"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-00bb6a80f01f03502"
+  instance_type = "t2.micro"
+ connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./terraform-deploy.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo touch /tmp/ssp.txt" 
+      ]
+  }
+}
