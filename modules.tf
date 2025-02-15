b11a7191173a119be77d1eecc887f3a16fff9b5e
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-15 19:29:57 +0530

    Create modules.tf

---
+++ modules.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYLZZKG25CI677AWM"
+  secret_key = "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"
+}
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+}
+
+===============================================================
