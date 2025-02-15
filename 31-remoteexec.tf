6ed41252e0906c9f2d3f617aa0228277edca674f
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-15 13:09:56 +0530

    Update 31-remoteexec.tf

--- 31-remoteexec.tf
+++ 31-remoteexec.tf
@@ -1,15 +1,19 @@
 #note chmod 400 pemfile is required in cases of permissions issues on local server
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "us-east-1"
   access_key = "AKIAYLZZKG25CI677AWM"
   secret_key = "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"
 }
 resource "aws_instance" "web" {
-  ami           = "ami-00bb6a80f01f03502"
+  ami           = "ami-053a45fff0a704a47"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-01361a5e0e1c4a69d"]
+  tags = {
+    name = "automationserver"
+  }
  connection {
     type     = "ssh"
-    user     = "ubuntu"
+    user     = "ec2-user"
     private_key = file("./terraform-deploy.pem")
     host     = self.public_ip
   }
