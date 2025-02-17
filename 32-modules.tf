8982a42b9b382ed150719e0851e059c6784fa1f0
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-17 16:08:11 +0530

    Update 32-modules.tf

--- 32-modules.tf
+++ 32-modules.tf
@@ -9,3 +9,13 @@ module "ec2_instance" {
 }
 
 ===============================================================
+# Deploys EKS Cluster with default propoerties
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYLZZKG25CI677AWM"
+  secret_key = "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"
+}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.33.1"
+}
