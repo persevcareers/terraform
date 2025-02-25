606074d2fb3289107d0bb033a9a38db6a2b80e19
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-25 16:06:29 +0530

    Update 41-sensitive.tf

--- 41-sensitive.tf
+++ 41-sensitive.tf
@@ -50,3 +50,40 @@ resource "local_sensitive_file" "automation" {
   content = "hello"
   filename = "automation.txt"
 }
+=================================================================
+
+#In case if we refer data from localsensitive file , and try to output the data , it will throw an error,
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+resource "local_sensitive_file" "automation" {
+  content = "hello"
+  filename = "automation.txt"
+}
+
+
+output "filecontent" {
+  value = local_sensitive_file.automation.content
+}
+================================================================================
+
+#To solve the above , we need to add sensitive = true
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+resource "local_sensitive_file" "automation" {
+  content = "hello"
+  filename = "automation.txt"
+}
+
+
+output "filecontent" {
+  value = local_sensitive_file.automation.content
+  sensitive = true
+}
