d285d1a9d0e9199a52403dbfe9824708b19ff34a
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-02-25 15:40:27 +0530

    Update 41-sensitive.tf

--- 41-sensitive.tf
+++ 41-sensitive.tf
@@ -1,10 +1,21 @@
+#Secure data using sesitive variable feature
 provider "aws" {
-  alias  = "ap_south"
   region     = "ap-south-1"
   access_key = ""
   secret_key = ""
 }
 
+variable "secret" {
+ sensitive = true
+ default = "password123"
+  
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
 variable "secret" {
  # sensitive = true
  default = "password123"
@@ -13,5 +24,29 @@ variable "secret" {
 
 resource "local_file" "automation" {
   content = var.secret
+  filename = "automation.txt"
+}
+=====================================================
+# Data will not be secured if we deploy using localfile generally ,
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+resource "local_file" "automation" {
+  content = "hello"
+  filename = "automation.txt"
+}
+
+==================================================
+# Data will not be secured if we deploy using localfile generally ,
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+resource "local_sensitive_file" "automation" {
+  content = "hello"
   filename = "automation.txt"
 }
